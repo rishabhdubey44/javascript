a0bc344567a8e4a1e461601dd33781cae1c21dfc
Rishabh <rishabhdubey2007@gmail.com>
2026-08-12 21:10:59 +0530

    commit

---
+++ 2.js
@@ -0,0 +1,83 @@
+/*function sum(n){
+ let total=0;
+ for(let i=1;i<=n;i++){
+    total+=i;
+ }
+  console.log(total);
+}
+sum(10);*/
+
+/*const users=[
+    {name:"Ram",age:21},
+    {name:"Raman",age:22}]
+console.log(users[0].name)
+
+const user={
+    name:"Ram",
+    age:19,
+    address:
+    {state:"Gujarat",
+        pincode:"396060"
+    }
+}
+console.log(user)
+console.log(user.address)
+console.log(user.address.state)*/
+
+/*let a="Ram";
+console.log(typeof(a));
+let x=Number(a);
+console.log(x,typeof(x));*/
+
+/*let x=0;
+console.log(typeof(x))
+console.log(Boolean(x))
+console.log(Boolean(""))
+console.log(Boolean("hi"))*/
+
+
+/*console.log("5"==5);
+console.log("5"===5);
+console.log(1==false);
+console.log(null==undefined);
+console.log(null===undefined);*/
+
+// grade
+/*let marks=90;
+if (marks >=90){
+    console.log('Grade A')
+}
+else if(marks>=80){
+    console.log('Grade B')
+}
+else{
+    console.log("fail")
+}*/
+
+const day=3
+
+switch(day){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("Invalid") 
+}
